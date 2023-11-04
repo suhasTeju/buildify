fd39f4e5aac8d54a6961469f133247fb77e2e514
suhasTeju <suhasteju84@gmail.com>
2023-11-04 13:01:43 +0530

    Add: Service file

---
+++ src/components/services/Service.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Service.module.scss"
+
+interface ServiceProps {
+
+}
+
+const Service:React.FC<ServiceProps> = () => {
+
+    return <div className={styles.serviceContainer}>
+        <div></div>
+        <div></div>
+    </div>
+}
